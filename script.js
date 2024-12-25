b4c38bbbe12349ca4aeff8e78600d54b0163acfd
Nimira43 <n.rabone72@aol.com>
2024-12-25 01:18:02 +0000

    Initialising calculator once the DOM is fully loaded

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const calculator = document.querySelector('.calculator')
+  const output = document.querySelector('.output')
+  const buttons = document.querySelector('.buttons')
+  const operators = ['+', '-', '*', '/']
+})
