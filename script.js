07469ab828ed6b19c6a2d27e7019914fec3c4772
Nimira43 <n.rabone72@aol.com>
2024-12-25 02:03:05 +0000

    Setting up else if when trigFunctions buttons are pressed

--- script.js
+++ script.js
@@ -22,6 +22,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       } else if (buttonValue === 'C') {
         displayValue = '0'
+
+
+      } else if (trigFunctions.includes(buttonValue)) {
+      
+
+
+
+
+
       } else {
         const lastChar = displayValue.slice(-1)
         
