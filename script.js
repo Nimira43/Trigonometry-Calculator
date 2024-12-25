3e55ce0450e54ce3e0f8a7dacc02d407040dd04b
Nimira43 <n.rabone72@aol.com>
2024-12-25 01:30:09 +0000

    Wrote the rest of the JS to create a functioning normal calculator

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const calculator = document.querySelector('.calculator')
   const output = document.querySelector('.output')
-  const buttons = document.querySelector('.buttons')
+  const buttons = document.querySelectorAll('.btn')
   const operators = ['+', '-', '*', '/']
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const buttonValue = button.innerText
+      let displayValue = output.innerText
+    
+      if (displayValue === '0') {
+        displayValue = ''
+      }
+
+      if (buttonValue === '=') {
+        displayValue = eval(displayValue)
+      } else if (buttonValue === 'C') {
+        displayValue = '0'
+      } else {
+        const lastChar = displayValue.slice(-1)
+        
+        if (operators.includes(buttonValue)) {
+          if (operators.includes(lastChar)) {
+            displayValue = displayValue.slice(0, -1)
+          } else {
+            displayValue = eval(displayValue)
+          }
+        }
+        displayValue += buttonValue
+      }
+      output.innerText = displayValue
+    })
+  })
 })
